1ee9c1840063b6079e542d110744ce08585c90a8
gitofgist <rishavmehra61@gmail.com>
2025-08-11 01:18:47 +0530

    problem subscriber added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use std::fs;
+use std::{fs, time::Duration, error::Error};
+use actix_web::{dev::Response, rt::time::sleep};
 use serde::{self, Deserialize};
+use solana_client::{
+    nonblocking::pubsub_client::PubsubClient, 
+    rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter}
+};
+use solana_sdk::commitment_config::CommitmentConfig;
+use futures::StreamExt;
+
 
 #[derive(Deserialize)]
 struct EnvConfig{
@@ -9,7 +17,8 @@ struct EnvConfig{
 #[derive(Deserialize)]
 struct Config {
     MAINNET_RPC_URL: String,
-    MAINNET_WSS_URL: String
+    MAINNET_WSS_URL: String,
+    RAYDIUM_LPV4: String
 }
 
 fn main(){
@@ -22,8 +31,56 @@ fn main(){
     
 }
 
-async fn subscribe()-> Result<()>{
+async fn subscribe() -> Result<(), Box<dyn Error>> {
+    let mut attempts =0;
+    loop {
+        attempts += 1;
+        let ws_client_result = PubsubClient::new(&get_config().env.MAINNET_WSS_URL.as_str()).await;
+    
+        match ws_client_result {
+            Ok(ws_client) =>{
+                if attempts == 1 {
+                    println!("Connected to Websocket.")
+                } else {
+                    println!("Connected ot Websocket after {attempts} attempts")
+                }
+                attempts = 0;
+                let (mut launch, _) = ws_client
+                    .logs_subscribe(
+                        RpcTransactionLogsFilter::Mentions(vec![get_config().env.RAYDIUM_LPV4.to_string()]),
+                        RpcTransactionLogsConfig {
+                            commitment: Some(CommitmentConfig::finalized()),
+                        },
+                    )
+                    .await?;
 
+                println!("Subscribed to Raydium Liquidity pool");
+
+                loop {
+                    match launch.next().await {
+                        Some(res) =>{
+                            // todo: need to add the message parsing 
+                            println!("Received log: {:?}", res);
+                        }
+                        None =>{
+                            println!("Steam ended");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Err(e)=>{
+                eprintln!("Failed to connect  to websocke. Attempts{attempts} of 10. Error: {e}");
+                if attempts >= 10 {
+                    eprintln!("Max retry limit reached");
+                    return  Err("Max limit reached out!".into());
+                }
+                sleep(Duration::from_secs(5)).await;
+            }
+        }
+    
+    }
 
 }
 
@@ -33,4 +90,8 @@ fn get_config() -> EnvConfig {
     let config = toml::from_str(&config_file).expect("Env are not spcified");
     config
 }
+
+
+
+
 
